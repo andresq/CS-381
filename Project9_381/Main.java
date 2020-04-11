68c33531e474046acbcacce6f5abbe84110b9d68
Andres Quintero <hello@andresq.com>
2020-04-11 16:06:12 -0400

    initial commit

---
+++ Project9_381/Main.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.*;
+import java.lang.Math;
+
+public class Main{
+    public static void main(String argv[]){
+        System.out.println("hello world");
+    }
+}
