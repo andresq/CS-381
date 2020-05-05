363c9b7be68e1a34f4a5cdf8cacf1d6dfb08b90d
Andres Quintero <hello@andresq.com>
2020-05-04 22:38:12 -0400

    adds data structures

--- Project11_381/main.cpp
+++ Project11_381/main.cpp
@@ -4,8 +4,62 @@
 using namespace std;
 
 
+// Data stuctures
+class imagePP{
+    public:
+    int numRows;
+    int numCols;
+    int minVal;
+    int maxVal;
+
+    class box{
+        public:
+        int minR;
+        int minC;
+        int maxR;
+        int maxC;
+    };
+
+    class boxNode{
+        public:
+        int boxType; // 1=doc box, 2=paragraph, 3= textLine
+        box BBOX;
+        boxNode* next;
+    };
+
+    class boxQ{
+        public:
+        boxNode* Qfront;
+        boxNode* Qback;
+        void insert(boxNode* newBoxNode){
+            Qback->next = newBoxNode;
+            Qback = newBoxNode;
+        }
+    };
+
+    int** imgAry;
+    int thresholdVal;
+
+    int* HPP;
+    int* VPP;
+    
+    int* HPPbin;
+    int* VPPbin;
+
+    int* HPPMorph;
+    int* VPPMorph;
+
+    int HPPruns;
+    int VPPruns;
+
+};
+
+
 
 int main(int argc, char* argv[]){
+    ifstream inFile(argv[1]);
+    ofstream outFile1(argv[2]);
+    ofstream outFile2(argv[3]);
 
 
 
@@ -14,5 +68,10 @@ int main(int argc, char* argv[]){
 
 
 
+
+
+    inFile.close();
+    outFile1.close();
+    outFile2.close();
     return 0;
 }
